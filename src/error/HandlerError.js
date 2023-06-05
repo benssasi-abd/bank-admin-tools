62e37557e3fcb2552e373220396d86129fa48147
benssasi-abd <126565178+benssasi-abd@users.noreply.github.com>
2023-06-05 21:14:18 +0100

    Update HandlerError.js

--- src/error/HandlerError.js
+++ src/error/HandlerError.js
@@ -1,9 +1,9 @@
 export class HandleError extends Error {
-  constructor(response: any) {
-    // if (response.data?.error) {
-    //   super(response.data.error[Object.keys(response.data.error)[0]]);
-    // } else {
-    //   super(response.data[Object.keys(response.data)[0]]);
-    // }
-  }
+  // constructor(response: any) {
+  //   // if (response.data?.error) {
+  //   //   super(response.data.error[Object.keys(response.data.error)[0]]);
+  //   // } else {
+  //   //   super(response.data[Object.keys(response.data)[0]]);
+  //   // }
+  // }
 }
