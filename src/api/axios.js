db37fa6c2ddfde41d4f36f350a22b21bf26218ef
benssasi-abd <126565178+benssasi-abd@users.noreply.github.com>
2023-06-06 00:05:11 +0100

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -57,6 +57,9 @@ console.log('getAPIClient');
     headers: {
       'Content-type': 'application/json',
       'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+      'Access-Control-Allow-Methods': 'PUT, POST, PATCH, DELETE, GET',
+
       // 'Authorization': `Bearer ${accessToken?.accessToken}`,
     },
   });
