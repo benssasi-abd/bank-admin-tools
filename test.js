00dc0b77892cadd58d97c11a208606c600db50d5
benssasi-abd <126565178+benssasi-abd@users.noreply.github.com>
2023-06-06 12:42:11 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,9 @@
+axios.post('https://ihold.twinsgroupe.com/api/oauth/tokens', {
+  username: 'admin.test@iholdbank.digital',
+  password: '@Marco123',
+  remember: false,
+  client_id: '967aa059-cd2f-4cad-8209-9592df0b9e5e',
+  client_secret: 'XuI3N8ldjPPea9KaJceHqm9IYa54ovraUBvNvZIl',
+  grant_type: 'password_web',
+  admin: true,
+});
