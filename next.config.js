6cfe335e08b38d09ffec8c7a76e0ace9ad1d7247
benssasi-abd <126565178+benssasi-abd@users.noreply.github.com>
2023-06-06 12:49:56 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -17,7 +17,7 @@ module.exports = withTM({
   env: {
     HOST_API_KEY: '',
     // FIREBASE AUTH
-    
+
     FIREBASE_API_KEY: '',
     FIREBASE_AUTH_DOMAIN: '',
     FIREBASE_PROJECT_ID: '',
@@ -35,6 +35,24 @@ module.exports = withTM({
     MAPBOX: '',
   },
   distDir: 'build',
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 });
 
 
